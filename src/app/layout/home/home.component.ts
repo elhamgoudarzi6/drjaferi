a97ebe916da98a589bf65220071382030a3a8fc7
elhamgoudarzi6 <elhamgoudarzi6@gmail.com>
2024-03-18 01:19:08 +0330

    12/27

--- src/app/layout/home/home.component.ts
+++ src/app/layout/home/home.component.ts
@@ -6,12 +6,13 @@ import { FaqComponent } from './faq/faq.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { HeaderComponent } from "../shared/header/header.component";
 import { FooterComponent } from "../shared/footer/footer.component";
+import { ServiceComponent } from './service/service.component';
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, FooterComponent,AboutComponent, HeroComponent, GalleryComponent, ContactComponent, FaqComponent]
+    imports: [HeaderComponent, FooterComponent,AboutComponent, HeroComponent, GalleryComponent, ContactComponent, FaqComponent,ServiceComponent]
 })
 export class HomeComponent {
 
